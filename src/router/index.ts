c9451d26b55969ce8997565bb0d1b55900874162
Luciano Rodriguez <231097lr@gmail.com>
2024-10-31 20:01:22 -0300

    primer commit

---
+++ src/router/index.ts
@@ -0,0 +1,45 @@
+import HomePage from "@/modules/landing/pages/HomePage.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+
+const router = createRouter({
+  history: createWebHistory( import.meta.env.BASE_URL ),
+  routes: [
+    {
+      path: '/',
+      name: 'landing',
+      component: () => import('@/modules/landing/layouts/LandingLayout.vue'),
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomePage,
+        },
+        {
+          path: '/features',
+          name: 'features',
+          component: () => import('@/modules/landing/pages/FeaturesPage.vue'),
+        },
+        {
+          path: '/pricing',
+          name: 'pricing',
+          component: () => import('@/modules/landing/pages/PricingPage.vue'),
+        },
+        {
+          path: '/contact',
+          name: 'contact',
+          component: () => import('@/modules/landing/pages/ContactPage.vue'),
+        },
+      ]
+    },
+
+    // auth
+
+    {
+      path: '/auth',
+      component: () => import('@/modules/auth/pages/LoginPage.vue')
+    }
+  ]
+})
+
+export default router;
